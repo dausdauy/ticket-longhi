cf50d76f7c89c7372dd2b4a0eb4613ef3900ad9a
dausdauy <dausdauy@gmail.com>
2020-07-23 16:52:14 +0700

    updated cookie

--- config/session.php
+++ config/session.php
@@ -124,10 +124,11 @@ return [
     |
     */
 
-    'cookie' => env(
-        'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
-    ),
+    'cookie' => 'laravel_session',
+    // 'cookie' => env(
+    //     'SESSION_COOKIE',
+    //     Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+    // ),
 
     /*
     |--------------------------------------------------------------------------
